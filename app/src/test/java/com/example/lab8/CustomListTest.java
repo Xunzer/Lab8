49062d2df3b34d042e51eb3528391b2aa87ff9a9
Bipbos <45457731+Xunzer@users.noreply.github.com>
2024-03-05 17:34:33 -0700

    updated

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,20 +11,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-    /**
-     * this gets size of the list
-     * @return
-     */
-    public int getCount(){
-        return cities.size();
-    }
-    /**
-     * this adds a city object to the list
-     *for the first phase it will be
-     empty * @param city
-     */
-    public void addCity(City city){
-    }
 
     private CustomList list;
     /**
